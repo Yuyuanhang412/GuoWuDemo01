18c3142fd7334a742e2b78a7fcd15876d1237272
粽子521 <1139319562@qq.com>
2016-10-25 15:00:10 +0800

    2

--- app/src/main/java/com/bwie/guowudemo01/fragment/Bumen.java
+++ app/src/main/java/com/bwie/guowudemo01/fragment/Bumen.java
@@ -1,7 +1,10 @@
 package com.bwie.guowudemo01.fragment;
 
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
 import android.support.annotation.Nullable;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -10,7 +13,16 @@ import android.widget.ListView;
 
 import com.bwie.guowudemo01.R;
 import com.bwie.guowudemo01.adapter.BumenAdapter;
+import com.bwie.guowudemo01.bean.Bean;
+import com.google.gson.Gson;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.ProtocolException;
+import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -21,6 +33,22 @@ public class Bumen extends android.support.v4.app.Fragment {
 
     private GridView gv;
     private List<String> list=new ArrayList<String>();
+    private String weekurl="http://open.qyer.com/qyer/bbs/entry?client_id=qyer_android&client_secret=9fcaae8aefc4f9ac4915";
+
+    Handler handler=new Handler(){
+        @Override
+        public void handleMessage(Message msg) {
+
+            Bean weekBean= (Bean) msg.obj;
+
+
+
+
+            List<Bean.DataEntity.ForumListEntity>list=weekBean.getData().getForum_list();
+            gv.setAdapter(new BumenAdapter(getActivity(),list));
+
+        }
+    };
 
     @Nullable
     @Override
@@ -34,18 +62,52 @@ public class Bumen extends android.support.v4.app.Fragment {
     public void onActivityCreated(@Nullable Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
 
-        addData();
+
 
         gv= (GridView) getActivity().findViewById(R.id.gv_fragm_bumen);
-        gv.setAdapter(new BumenAdapter(getActivity(),list));
+
+
+
+
+        new Thread(){
+
+            @Override
+            public void run() {
+
+                try {
+                    URL url = new URL(weekurl);
+
+                    HttpURLConnection httpURLConnection = (HttpURLConnection) url.openConnection();
+                    httpURLConnection.setRequestMethod("GET");
+                    httpURLConnection.connect();
+
+                    if (httpURLConnection.getResponseCode() == 200) {
+
+                        InputStream inputStream = httpURLConnection.getInputStream();
+
+                        //解析Gson
+                        Gson gson = new Gson();
+                        Bean weekbean = gson.fromJson(new InputStreamReader(inputStream), Bean.class);
+                        Log.i("-------", weekbean.toString());
+                        Message msg = Message.obtain();
+                        msg.obj = weekbean;
+
+                        handler.sendMessage(msg);
+
+                    }
+
+
+                } catch (MalformedURLException e) {
+                    e.printStackTrace();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+
+            };
+        }.start();
+
+
+
     }
 
-   private void addData() {
-        for(int i=0;i<30;i++){
-
-            list.add("item"+i);
-
-        }
-
-   }
 }
--- app/src/main/java/com/bwie/guowudemo01/fragment/Lianbo.java
+++ app/src/main/java/com/bwie/guowudemo01/fragment/Lianbo.java
@@ -1,7 +1,10 @@
 package com.bwie.guowudemo01.fragment;
 
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.os.Message;
 import android.support.annotation.Nullable;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -9,7 +12,16 @@ import android.widget.ListView;
 
 import com.bwie.guowudemo01.R;
 import com.bwie.guowudemo01.adapter.ZhenWuAdapter;
+import com.bwie.guowudemo01.bean.Bean;
+import com.bwie.guowudemo01.utils.HttpGet;
+import com.google.gson.Gson;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -18,8 +30,11 @@ import java.util.List;
  */
 public class Lianbo extends android.support.v4.app.Fragment {
 
-    private List<String> list=new ArrayList<String>();
+    private boolean isViewCreated;
+    private boolean isLoadDataCompleted;
+
     private ListView lv;
+    private String weekurl="http://open.qyer.com/qyer/bbs/entry?client_id=qyer_android&client_secret=9fcaae8aefc4f9ac4915";
 
     @Nullable
     @Override
@@ -35,20 +50,69 @@ public class Lianbo extends android.support.v4.app.Fragment {
         super.onActivityCreated(savedInstanceState);
 
         addData();
+
+        initView();
+
+
+    }
+
+    private void initView() {
+
         lv= (ListView) getActivity().findViewById(R.id.lv_fragm_zhengwu);
-        lv.setAdapter(new ZhenWuAdapter(getActivity(),list));
+       // lv.setAdapter(new ZhenWuAdapter(getActivity(),list));
+        isViewCreated=true;
 
     }
 
     private void addData() {
 
-        for(int i=0;i<30;i++){
+        new AsyncTask<Void,Void,String>(){
+            @Override
+            protected String doInBackground(Void... params) {
+                String str = HttpGet.Getstr(weekurl);
+                return str;
+            }
 
-            list.add("text"+i);
+            @Override
+            protected void onPostExecute(String s) {
+                super.onPostExecute(s);
 
-        }
+                Gson gson = new Gson();
+                Bean bean = gson.fromJson(s, Bean.class);
+                List<Bean.DataEntity.ForumListEntity.GroupEntity> list = bean.getData().getForum_list().get(0).getGroup();
+
+                //适配器
+                lv.setAdapter(new ZhenWuAdapter(getActivity(), list));
+            }
+        }.execute();
 
     }
 
 
+    @Override
+    public void setUserVisibleHint(boolean isVisibleToUser) {
+        super.setUserVisibleHint(isVisibleToUser);
+
+        if (isVisibleToUser && isViewCreated)//只有在用户可见以及初始化之后才加载数据
+        {
+            lazyLoad();
+        }
+
+    }
+
+    private void lazyLoad(){
+        if (!isLoadDataCompleted){
+            initView();
+            addData();
+            isLoadDataCompleted=true;
+
+        }
+    }
+
+    //这里需要重置状态，不然加载了之后就没办法再重新加载了
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        isLoadDataCompleted=false;
+    }
 }
